48181364ba8a4afa0c47307bda7239e2f538fda3
mocilnikcarlos <mocilnik.web@gmail.com>
2022-07-07 17:36:32 -0300

    chore(Root): modification map btnnavbar

--- components/common/header/navbar.js
+++ components/common/header/navbar.js
@@ -3,44 +3,22 @@ import BtnNavbar from "./btnNavbar";
 
 import style from "./style.module.scss";
 
-const getLink = {
-  getNavLink: [
-    {
-      id: 1,
-      target: "Inicio",
-      to: "/",
-    },
-    {
-      id: 2,
-      target: "Servicio",
-      to: "/service",
-    },
-    {
-      id: 3,
-      target: "Proyectos",
-      to: "/projects",
-    },
-    {
-      id: 4,
-      target: "Sobre Mi",
-      to: "/aboutme",
-    },
-    {
-      id: 5,
-      target: "Contacto",
-      to: "/contact",
-    },
-  ],
-};
+const getLink = [
+  { id: 1, target: "home", page: "/" },
+  { id: 2, target: "service", page: "/service" },
+  { id: 3, target: "project", page: "/project" },
+  { id: 4, target: "aboutme", page: "/aboutme" },
+  { id: 5, target: "contact", page: "/contact" },
+];
 
 export default function Navbar() {
   return (
     <>
       <nav>
         <ul className={style.listlink}>
-          {getLink.getNavLink.map((item) => (
+          {getLink.map((item) => (
             <li key={item.id}>
-              <BtnNavbar placeholder={item.target} page={item.to} />
+              <BtnNavbar placeholder={item.target} page={item.page} />
             </li>
           ))}
         </ul>
