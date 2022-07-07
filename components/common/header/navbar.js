f6ff4c93b189b15c6697ea454d29281bade5d13e
mocilnikcarlos <mocilnik.web@gmail.com>
2022-07-07 17:24:29 -0300

    fix(Root): Fix btnnavbar

--- components/common/header/navbar.js
+++ components/common/header/navbar.js
@@ -1,14 +1,44 @@
-import dataNav from "helpers/dataNavLinks.json";
+// import dataNav from "helpers/dataNavLinks";
 import BtnNavbar from "./btnNavbar";
 
 import style from "./style.module.scss";
+
+const getLink = {
+  getNavLink: [
+    {
+      id: 1,
+      target: "Inicio",
+      to: "/",
+    },
+    {
+      id: 2,
+      target: "Servicio",
+      to: "/service",
+    },
+    {
+      id: 3,
+      target: "Proyectos",
+      to: "/projects",
+    },
+    {
+      id: 4,
+      target: "Sobre Mi",
+      to: "/aboutme",
+    },
+    {
+      id: 5,
+      target: "Contacto",
+      to: "/contact",
+    },
+  ],
+};
 
 export default function Navbar() {
   return (
     <>
       <nav>
         <ul className={style.listlink}>
-          {dataNav.getNavLink.map((item) => (
+          {getLink.getNavLink.map((item) => (
             <li key={item.id}>
               <BtnNavbar placeholder={item.target} page={item.to} />
             </li>
