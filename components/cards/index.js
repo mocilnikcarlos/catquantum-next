fa33ef096cb59475ce786b0c8c4d5154466074c1
mocilnikcarlos <mocilnik.web@gmail.com>
2022-07-09 08:09:40 -0300

    feat(Root): Added cards

--- components/cards/index.js
+++ components/cards/index.js
@@ -1,21 +1,41 @@
-import style from "./style.module.scss";
+import work from "./work.module.scss";
+import line from "./line.module.scss";
 
-export default function CardWork({ title, text, icon }) {
+const CardWork = ({ title, text, icon }) => {
   return (
     <>
-      <div className={style.boxcircledesign}>
-        <div className={style.contentcircledesign}>
-          <div className={style.iconcircle}>
-            <div className={style.icon}>{icon}</div>
+      <div className={work.boxcircledesign}>
+        <div className={work.contentcircledesign}>
+          <div className={work.iconcircle}>
+            <div className={work.icon}>{icon}</div>
           </div>
-          <p className={`${style.titlecircle} ${style.titlecircleup}`}>
-            {text}
-          </p>
-          <h3 className={`${style.titlecircle} ${style.titlecircledown}`}>
+          <p className={`${work.titlecircle} ${work.titlecircleup}`}>{text}</p>
+          <h3 className={`${work.titlecircle} ${work.titlecircledown}`}>
             {title}
           </h3>
         </div>
       </div>
     </>
   );
-}
+};
+
+const CardLines = ({ icon, title, text }) => {
+  return (
+    <div className={line.cardline}>
+      <span className={line.linehover}></span>
+      <span className={line.linehover}></span>
+      <span className={line.linehover}></span>
+      <span className={line.linehover}></span>
+      <div className={line.contentcardline}>
+        <div className={line.contenttitlecardline}>
+          <div className={line.iconcardline}>{icon}</div>
+          <p className={line.titlecardline}>{title}</p>
+        </div>
+
+        <p className={line.textcardline}>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export { CardLines, CardWork };
