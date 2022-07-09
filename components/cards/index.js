fdaf417568d59454895b3ee91b4a0459a2eea07a
mocilnikcarlos <mocilnik.web@gmail.com>
2022-07-09 11:15:54 -0300

    feat(Root): Added card and style

--- components/cards/index.js
+++ components/cards/index.js
@@ -1,5 +1,7 @@
 import work from "./work.module.scss";
 import line from "./line.module.scss";
+import img from "./img.module.scss";
+import EyeCard from "assets/svg/eye";
 
 const CardWork = ({ title, text, icon }) => {
   return (
@@ -21,21 +23,61 @@ const CardWork = ({ title, text, icon }) => {
 
 const CardLines = ({ icon, title, text }) => {
   return (
-    <div className={line.cardline}>
-      <span className={line.linehover}></span>
-      <span className={line.linehover}></span>
-      <span className={line.linehover}></span>
-      <span className={line.linehover}></span>
-      <div className={line.contentcardline}>
-        <div className={line.contenttitlecardline}>
-          <div className={line.iconcardline}>{icon}</div>
-          <p className={line.titlecardline}>{title}</p>
-        </div>
+    <>
+      <div className={line.cardline}>
+        <span className={line.linehover}></span>
+        <span className={line.linehover}></span>
+        <span className={line.linehover}></span>
+        <span className={line.linehover}></span>
+        <div className={line.contentcardline}>
+          <div className={line.contenttitlecardline}>
+            <div className={line.iconcardline}>{icon}</div>
+            <p className={line.titlecardline}>{title}</p>
+          </div>
 
-        <p className={line.textcardline}>{text}</p>
+          <p className={line.textcardline}>{text}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export { CardLines, CardWork };
+const FirstCardDesigner = ({ title, text, src }) => {
+  return (
+    <>
+      <div className={img.imgcardfirst}>
+        <div className={img.boxeye}>
+          <span className={img.iconeye}>
+            <EyeCard />
+          </span>
+        </div>
+        <div className={img[`${src}`]}></div>
+        <div className={img.textcard}>
+          <span className={img.titleupcard}>{title}</span>
+          <span className={img.titledowncard}>{text}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const SecondCardDesigner = ({ title, text, src }) => {
+  return (
+    <>
+      <div className={img.imgcardsecond}>
+        <div className={img.boxeye}>
+          <span className={img.iconeye}>
+            <EyeCard />
+          </span>
+        </div>
+        <div className={img[`${src}`]}></div>
+        <div className={img.textcard}>
+          <span className={img.titleupcard}>{title}</span>
+          <span className={img.titledowncard}>{text}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { CardLines, CardWork, FirstCardDesigner, SecondCardDesigner };
