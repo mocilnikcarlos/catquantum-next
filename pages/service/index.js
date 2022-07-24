d577d4d12e13f0f81561b166e80d7b4fd9fab1c4
mocilnikcarlos <mocilnik.web@gmail.com>
2022-07-24 10:30:50 -0300

    feat(Root): Added page service

---
+++ pages/service/index.js
@@ -0,0 +1,12 @@
+import Article from "components/article";
+import Section from "components/sections";
+
+export default function Service(second) {
+  return (
+    <>
+      <Section>
+        <Article></Article>
+      </Section>
+    </>
+  );
+}
