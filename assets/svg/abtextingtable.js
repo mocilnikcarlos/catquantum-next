eb30b0c2e36f7fa92e50e71cbb9372ddb7cfeb8f
mocilnikcarlos <mocilnik.web@gmail.com>
2022-07-24 16:44:13 -0300

    feat(Root): Added Icon for table service

---
+++ assets/svg/abtextingtable.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const AbTestingTable = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={29} height={30} {...props}>
+    <path
+      d="M2.9 0A2.951 2.951 0 0 0 0 3v12h2.9V9h2.9v6h2.9V3a2.951 2.951 0 0 0-2.9-3H2.9m0 3h2.9v3H2.9M29 20.25V18a2.951 2.951 0 0 0-2.9-3h-5.8v15h5.8a2.951 2.951 0 0 0 2.9-3v-2.25a2.273 2.273 0 0 0-2.175-2.25A2.273 2.273 0 0 0 29 20.25M26.1 27h-2.9v-3h2.9v3m0-6h-2.9v-3h2.9M5.5 29.415l-2.3-1.83 20.3-27 2.3 1.83Z"
+      fill="#aeb3ba"
+    />
+  </svg>
+);
+
+export default AbTestingTable;
---
+++ assets/svg/prototipetable.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const PrototipeTable = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} {...props}>
+    <path
+      d="M10 15H6.22a3.333 3.333 0 1 1 0-3.333H10V3.333A3.333 3.333 0 0 1 13.333 0h13.334A3.333 3.333 0 0 1 30 3.333v23.334A3.333 3.333 0 0 1 26.667 30H13.333A3.333 3.333 0 0 1 10 26.667Zm3.333 0v11.667h13.334V3.333H13.333v8.333h3.78a3.333 3.333 0 1 1 0 3.333Z"
+      fill="#aeb3ba"
+      fillRule="evenodd"
+    />
+  </svg>
+);
+
+export default PrototipeTable;
